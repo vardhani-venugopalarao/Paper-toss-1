28b425c0bf4364ae99a8f74778bf8d48e6ad9a87
vardhani-venugopalarao <68939700+vardhani-venugopalarao@users.noreply.github.com>
2020-09-25 01:09:37 +0530

    Add files via upload

---
+++ Paper.js
@@ -0,0 +1,27 @@
+class Paper{
+    constructor(x,y,radius){
+        var options = {
+            isStatic: false,
+            restitution: 0.3,
+            friction: 0.5,
+            density: 1.2
+        }
+
+        this.body = Bodies.circle(x, y, radius, options);
+        this.radius = radius;
+
+        World.add(world, this.body);
+    }
+
+    display(){
+        var pos = this.body.position;
+        push();
+        fill("white");
+        translate(pos.x,pos.y);
+        rotate(this.body.angle);
+        ellipseMode(CENTER);
+        ellipse(0,0,this.radius*2);
+        pop();
+        
+    }
+}
